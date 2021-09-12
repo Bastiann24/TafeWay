9284d370ec2e5e38d0ad537647560f81408594ff
Bastian Ugalde <xxbastiannxx@gmail.com>
2021-09-12 18:36:17 +0930

    Testing for Github

---
+++ TafeWay/SecondViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SecondViewController.swift
+//  TafeWay
+//
+//  Created by Bastian Ugalde  on 13/8/21.
+//
+
+import Foundation
--- TafeWay/ViewController.swift
+++ TafeWay/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    
+    @IBAction func logInClicked(_ sender: UIButton) {
+        let alertController = UIAlertController (title: "LOGGED IN", message: String(format:"Welcome to TafeWay " + emailLabel.text!), preferredStyle: .alert)
+        
+        let alertAction =   UIAlertAction(title: "Thanks!", style: .default, handler: nil)
+        
+        alertController.addAction(alertAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
     }
-
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
 
 }
 
